0f5bc09db296d9e94bb88db9d3362a03695c67af
Stephan Hoogland <stephan@shoogland.com>
2018-07-02 17:02:03 +0200

    fixed pages error

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,49 +6,56 @@ import { ScanTicketPage } from '../scan-ticket/scan-ticket';
 import { SearchPage } from '../search/search';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
-  barcode: string;
-  error: string;
-  constructor(
-    public navCtrl: NavController,
-    private barcodeScanner: BarcodeScanner
-  ) {
+	barcode: string;
+	error: string;
+	pages: Array<{
+		title: string,
+		component: any,
+		class: string
+	}>;
 
-	this.pages = [
-		{
-			title: 'Scan Ticket',
-			component: ScanTicketPage,
-			class: 'bg-yellow'
-		},
-		{
-			title: 'Zoek kind',
-			component: SearchPage,
-			class: 'bg-blue'
-		},
-		{
-			title: 'Koppel kind aan hut',
-			class: 'bg-green'
-		}
-	];
+	constructor(
+		public navCtrl: NavController,
+		private barcodeScanner: BarcodeScanner
+	) {
 
-  }
+		this.pages = [
+			{
+				title: 'Scan Ticket',
+				component: ScanTicketPage,
+				class: 'bg-yellow'
+			},
+			{
+				title: 'Zoek kind',
+				component: SearchPage,
+				class: 'bg-blue'
+			},
+			{
+				title: 'Koppel kind aan hut',
+				component: null,
+				class: 'bg-green'
+			}
+		];
 
-  scanCode() {
-    console.log('scan the shizzle');
+	}
+
+	scanCode() {
+		console.log('scan the shizzle');
 		this.barcodeScanner.scan().then((barcodeData) => {
 			this.barcode = barcodeData.text;
 		}, (error) => {
-      console.log(error);
+			console.log(error);
 			this.error = error.message;
 		});
 	}
 	openPage(page) {
-      // Reset the content nav to have just this page
-      // we wouldn't want the back button to show in this scenario
+		// Reset the content nav to have just this page
+		// we wouldn't want the back button to show in this scenario
 
-      this.navCtrl.setRoot(page.component);
-    }
-  }
+		this.navCtrl.setRoot(page.component);
+	}
+}
