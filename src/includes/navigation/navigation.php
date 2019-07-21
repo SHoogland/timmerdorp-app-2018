569388d1570a0e84f22415078ea7b283a99a3952
Stan <stvanbaarsen@gmail.com>
2019-07-21 14:48:08 +0200

    remove obsolete property & undo error from last commit

--- src/includes/navigation/navigation.php
+++ src/includes/navigation/navigation.php
@@ -4,7 +4,7 @@
       <ion-icon name='menu'></ion-icon>
     </button>
     <ion-title>
-      <img src="assets/imgs/logo.png" onClick="javascript:document.getElementsByClassName('nav-items item item-block item-md')[0].click();//go to home page"align="middle" class="nav-logo">
+      <img src="assets/imgs/logo.png" onClick="javascript:document.getElementsByClassName('nav-items item item-block item-md')[0].click();//go to home page" class="nav-logo">
     </ion-title>
   </ion-navbar>
 </ion-header>
