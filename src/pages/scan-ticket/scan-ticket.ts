b79a1dd73b717f72dc2c5af95cd4402857ca4df0
Stan <stvanbaarsen@gmail.com>
2019-07-22 13:14:11 +0200

    bugfix

--- src/pages/scan-ticket/scan-ticket.ts
+++ src/pages/scan-ticket/scan-ticket.ts
@@ -4,7 +4,6 @@ import { Storage } from '@ionic/storage';
 import * as WPAPI from 'wpapi';
 import { HomePage } from '../home/home';
 import { LoginPage } from '../login/login';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 @Component({
 	selector: 'page-scan-ticket',
@@ -33,10 +32,7 @@ export class ScanTicketPage {
 		wristBandNr: string
 	};
 
-	barcode: string;
-
 	constructor(
-		private barcodeScanner: BarcodeScanner,
 		public navCtrl: NavController,
 		public navParams: NavParams,
 		public platform: Platform,
@@ -51,9 +47,6 @@ export class ScanTicketPage {
 			username: '',
 			password: ''
 		}
-
-		this.barcode = '';
-
 		this.ticket = {
 			barcode: '',
 			firstName: '',
@@ -88,55 +81,49 @@ export class ScanTicketPage {
 
 		this.init();
 
-		this.barcodeScanner.scan().then(barcodeData => {
-			console.log('Barcode data', barcodeData);
-			this.barcode = barcodeData.text;
-			Promise.all([
-				this.storage.get('username').then((val) => {
-					this.login.username = val;
-				}, (error) => {
-					this.login.username = '';
-				}),
-				this.storage.get('password').then((val) => {
-					this.login.password = val;
-				}, (error) => {
-					this.login.password = '';
-				})
-			]).then(() => {
-				var wp = this.getWpApi('barcode');
-				return wp.handler().param('barcode', this.barcode);
-			}).then((result) => {
-				if (result.code === 200) {
-					self.ticket.barcode = result.meta.WooCommerceEventsTicketID[0];
-					self.ticket.firstName = result.meta.WooCommerceEventsAttendeeName[0];
-					self.ticket.lastName = result.meta.WooCommerceEventsAttendeeLastName[0];
-					self.ticket.birthDate = result.meta['fooevents_custom_geboortedatum_(dd-mm-jjjj)'][0];
+		Promise.all([
+			this.storage.get('username').then((val) => {
+				this.login.username = val;
+			}, (error) => {
+				this.login.username = '';
+			}),
+			this.storage.get('password').then((val) => {
+				this.login.password = val;
+			}, (error) => {
+				this.login.password = '';
+			})
+		]).then(() => {
+			var wp = this.getWpApi('barcode');
+			return wp.handler().param('barcode', this.navParams.get('barcode'));
+		}).then((result) => {
+			if (result.code === 200) {
+				self.ticket.barcode = result.meta.WooCommerceEventsTicketID[0];
+				self.ticket.firstName = result.meta.WooCommerceEventsAttendeeName[0];
+				self.ticket.lastName = result.meta.WooCommerceEventsAttendeeLastName[0];
+				self.ticket.birthDate = result.meta['fooevents_custom_geboortedatum_(dd-mm-jjjj)'][0];
 
-					if (result.meta.wristband) {
-						self.modal.showModal = true;
-						self.ticket.wristBandNr = result.meta.wristband[0];
-						self.modal.text = 'Dit ticket heeft al een armband!';
-					}
+				if (result.meta.wristband) {
+					self.modal.showModal = true;
+					self.ticket.wristBandNr = result.meta.wristband[0];
+					self.modal.text = 'Dit ticket heeft al een armband!';
+				}
 
-					self.loading = false;
-				} else {
-					if (result.message == 'access denied') {
-						this.notLoggedIn = true;
-					} else {
-						self.error = result.message;
-						self.loading = false;
-					}
-				}
-			}).catch((error) => {
-				if (error.code === 'invalid_username' || error.code === 'incorrect_password') {
-					this.loginError = true;
-				} else {
-					self.error = error.message;
-				}
 				self.loading = false;
-			});
-		}).catch(err => {
-			console.log('Error', err);
+			} else {
+				if (result.message == 'access denied') {
+					this.notLoggedIn = true;
+				} else {
+					self.error = result.message;
+					self.loading = false;
+				}
+			}
+		}).catch((error) => {
+			if (error.code === 'invalid_username' || error.code === 'incorrect_password') {
+				this.loginError = true;
+			} else {
+				self.error = error.message;
+			}
+			self.loading = false;
 		});
 	}
 
@@ -147,7 +134,7 @@ export class ScanTicketPage {
 		var wp = this.getWpApi('add-wristband');
 		wp
 			.handler()
-			.param('barcode', this.barcode)
+			.param('barcode', this.navParams.get('barcode'))
 			.param('wristband', this.ticket.wristBandNr)
 			.then((result) => {
 				console.log(result);
